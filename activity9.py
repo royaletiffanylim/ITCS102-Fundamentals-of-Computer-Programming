0fc20221f14e04c82f0240e0c098560ae33cd7bf
royaletiffanylim <yaletiffanyplim@gmail.com>
2025-08-18 15:51:36 +0800

    initial commit

---
+++ activity9.py
@@ -0,0 +1,13 @@
+a = 25
+b = 25
+
+c = 9
+d = 10
+username = 'tiffany' 
+password = 'yale_mamamo25'
+print((username == 'tiffany') and (password == 'yale_mamamo25'))
+print( ( a > b) and ( c > d) ) 
+print( ( a > b) or ( c > d) ) 
+print(not(( a > b) or ( c > d))) 
+
+
