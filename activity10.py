d9a451379a16ac9fe1d92c3ed9fcadc70ae24552
royaletiffanylim <yaletiffanyplim@gmail.com>
2025-08-27 15:10:27 +0800

    initial commit

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "royale_tiffany25"
+password = "mamamoblue02"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password): 
+	print ("YOU SLAYED, MOMSH!")
+else: 
+	print("BETTER LUCK NEXT TIME, SIS!")
