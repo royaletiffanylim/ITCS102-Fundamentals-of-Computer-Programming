804259c80c39bd230e8115349003e24905f32651
royaletiffanylim <yaletiffanyplim@gmail.com>
2025-09-16 22:06:37 +0800

    initial commit

---
+++ code_challenge6.py
@@ -0,0 +1,10 @@
+print ("ODD NUMBER SUMMATION")
+
+sum = 0
+
+for yale in range (0, 10, 1) :
+	mamamo = eval(input("Enter Your Number --> "))
+	if mamamo % 2:
+		sum += mamamo
+
+print("The Sum of all ODD NUMBERS is", sum)
