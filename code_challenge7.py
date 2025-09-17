1f31e1e8b30ccc9229382dcc92f999b22c4c5374
royaletiffanylim <yaletiffanyplim@gmail.com>
2025-09-17 12:53:06 +0800

    initial commit

---
+++ code_challenge7.py
@@ -0,0 +1,8 @@
+print ("COUNTDOWN TIMER SIMULATOR")
+number = eval(input("Enter starting number for the Countdown:"))
+
+print ("Countdown Started:")  
+for n in range (number, 0, -1) :
+    print (n) 
+    
+print ("LIFTOFF!") 
