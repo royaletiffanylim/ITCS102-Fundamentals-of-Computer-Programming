ad0bc79455b6fea4b8654cd5cafec586de35744e
royaletiffanylim <yaletiffanyplim@gmail.com>
2025-10-12 16:43:26 +0800

    initial commit

--- activity19.py
+++ activity19.py
@@ -1,6 +1,6 @@
-for i in range(1, 11, 1):
-    for x in range(10, i, -1):
+for k in range(1, 11, 1):
+    for b in range(10, k, -1):
         print("☆", end = " ")
-    for y in range(1, i, 1):
+    for b in range(1, k, 1):
         print("♡", end = " ")
     print()
