9f26b7656e9485c4a15d8c81012846b49ffdb99c
royaletiffanylim <yaletiffanyplim@gmail.com>
2025-12-02 21:04:22 +0800

    initial commit

---
+++ activity21.py
@@ -0,0 +1,9 @@
+Lim = True
+while Lim == True:
+    quest = input("Do you love me? (yes/no) ")
+    if quest == "yes":
+        print("You're So Sweet <3")
+        continue
+    else:
+        print("That's So Sad :(")
+        break
